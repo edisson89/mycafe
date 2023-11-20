ec8682c2cfddfc8015f53b2a29d8526c45854e74
edisson89 <edisson.12@hotmail.com>
2023-11-20 17:03:26 -0500

    ajustes home

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ function App() {
   return (
     <>
     <div className={styles.app}>
-      <h1 className={styles.title}>Tienda De Cafe</h1>
+      <h1 className={styles.title}>Tienda De Cafe Especial</h1>
       <img src={image} alt="cafe" height='500' width='800' />
 
     </div>
     <div className={styles.contactenos}>
-      <h2>Contactenos:  +57 3003003000</h2>
-      <h3>Calle 53 con cra 51-77</h3>
+      <h2>Contactenos:  +57 3174782918</h2>
+      <h3>Bogota</h3>
     
     </div>
-    <div className={styles.bar  }>
+    <div className={styles.bar }>
       <a rel="stylesheet" href="facebook">Facebook</a>
       <a rel="stylesheet" href="facebook">Wasapp</a>
       <a rel="stylesheet" href="facebook">Tick tock</a>
