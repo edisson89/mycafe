8bc9c577d31c20289c5cbc5d51199da1537d6821
edisson89 <edisson.12@hotmail.com>
2023-11-20 22:16:09 -0500

    cambios en home

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import styles from  './app.module.css'
-import image from './assets/cafe.jpeg'
+//import { useRef } from 'react'
+import styles from "./app.module.css";
+import image from "./assets/cafe.jpeg";
+//import {  useScroll, animated, useSpring} from '@react-spring/web'
 
 function App() {
   return (
     <>
-    <div className={styles.app}>
-      <h1 className={styles.title}>Tienda De Cafe Especial</h1>
-      <img src={image} alt="cafe" height='500' width='800' />
-
-    </div>
-    <div className={styles.contactenos}>
-      <h2>Contactenos:  +57 3174782918</h2>
-      <h3>Bogota</h3>
-    
-    </div>
-    <div className={styles.bar }>
-      <a rel="stylesheet" href="facebook">Facebook</a>
-      <a rel="stylesheet" href="facebook">Wasapp</a>
-      <a rel="stylesheet" href="facebook">Tick tock</a>
-
-    </div>
+      <div className={styles.app}>
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>Tienda De Cafe Especial</h1>
+        </div>
+        <div className={styles.imageContainer}>
+          <img src={image} className={styles.image} alt="cafe" />
+        </div>
+      
+      <div className={styles.contactenos}>
+        <h2>Contactenos: +57 3174782918</h2>
+        <h3>Bogota</h3>
+      </div>
+      </div>
+      <div className={styles.bar}>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.facebook.com/profile.php?id=100083398416105"
+        >
+          Facebook
+        </a>
+        <a rel="stylesheet" href="facebook">
+          Wasapp
+        </a>
+        <a rel="stylesheet" href="facebook">
+          Tick tock
+        </a>
+      </div>
     </>
   );
 }
